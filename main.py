0627133533b6c04f2172907e58bc6ce57e8c25c7
SimonBerens <sberensnyc@gmail.com>
2019-01-12 11:11:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from os import urandom
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = urandom(64)
+socketio = SocketIO(app)
+
+
+def message_received(methods=["GET", "POST"]):
+    print("message was received!!!")
+
+
+@socketio.on("my event")
+def handle_my_custom_event(json, methods=["GET", "POST"]):
+    print("received my event: " + str(json))
+    socketio.emit("my response", json, callback=message_received)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
