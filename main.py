210b40ae4b9ed5721fe0cadd90ecfec9380ca123
DanielGelfand <dgelfand@stuy.edu>
2019-01-12 14:58:33 -0500

    everything

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask_socketio import SocketIO
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import requests
-
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
+from utils import authenticate
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = urandom(64)
@@ -20,15 +22,42 @@ def handle_my_custom_event(json, methods=["GET", "POST"]):
     print("received my event: " + str(json))
     socketio.emit("my response", json, callback=message_received)
 
-def get_cyberbullied():
-    url = "https://api.tisane.ai/parse"
-    post_fields = {"language":"en", "content":"Babylonians should not be allowed at managerial positions."}
-    headers = {'content-type': 'application/json','Ocp-Apim-Subscription-Key': '7eb4070eb5a7451d8d99d6767263a0d0'}
-    r = requests.post(url, data=json.dumps(post_fields), headers=headers)
-    json = urlopen(r).read().decode('utf-8')
-    print(json)
+def get_cyberbullied(content):
+    '''Returns api dict based on user msg'''
+    headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '7eb4070eb5a7451d8d99d6767263a0d0',
+    }
 
-get_cyberbullied()
+    params = urllib.parse.urlencode({})
+
+    try:
+        conn = http.client.HTTPSConnection('api.tisane.ai')
+        body = '{"language":"en", "content":' + '"' + content + '",' + '"settings":{}}'
+        print("This is the body",body)
+        conn.request("POST", "/parse?%s" % params, body ,headers)
+        response = conn.getresponse()
+        data = response.read()
+        dict = json.loads(data.decode('utf-8'))
+        print(dict)
+        return dict
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+#get_cyberbullied("You are an idiot.")
+
+
+def toFlag(content):
+    data = get_cyberbullied(content)
+    if(data['sentiment_expressions'][0]['polarity'] == 'negative' and (data['abuse'][0]['severity'] == "medium" or data['abuse'][0]['severity'] == "high" or data['abuse'][0]['severity'] == "extreme")):
+        print("You are getting flagged")
+        return True
+    return False
+
+toFlag("You are a fucking idiot")
+
 
 @app.route("/")
 def index():
